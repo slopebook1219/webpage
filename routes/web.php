e90b87fe3ce9827b45a7c2105a266d9c23c41c19
slopebook1219 <saka.ttt.1219@gmail.com>
2025-02-01 09:15:15 +0900

    投稿機能と保存機能の作成

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,5 @@ Route::get('/', function () {
 });
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create',  [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/edit',  [PostController::class, 'edit'])->name('posts.edit');
